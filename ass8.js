1cbd4371ffb5666a6df46fb40523b52fa16e6ef0
Daniella Caden <daniella.wwcd@gmail.com>
2021-10-04 19:46:44 +0100

    add firstpart

--- ass8.js
+++ ass8.js
@@ -1,6 +1,10 @@
-function details(name,age);
+function details(name,age)
 {
     console.log(name,age);
 }
 
-details("Daniella","20");
+details("Daniella","20"); 
+details("Treasure","18");
+details("Michaela","15");
+details("Jayden","9");
+details("Michael","20");
