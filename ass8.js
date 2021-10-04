f7d8211b3e27c9826bb84016b59e0f22412b3f95
Daniella Caden <daniella.wwcd@gmail.com>
2021-10-04 19:51:04 +0100

    add second

--- ass8.js
+++ ass8.js
@@ -8,3 +8,15 @@ details("Treasure","18");
 details("Michaela","15");
 details("Jayden","9");
 details("Michael","20");
+
+function first(second)
+{
+    second(name);
+}
+function second(name);
+{
+    console.log(name);
+}
+
+first(second);
+second("Daniella");
