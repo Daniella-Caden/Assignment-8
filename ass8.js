119a831aaf0c0f6c38ccdd658d542af974b34f95
Daniella Caden <daniella.wwcd@gmail.com>
2021-10-04 20:31:56 +0100

    add 13

--- ass8.js
+++ ass8.js
@@ -23,6 +23,22 @@ first();
 
 
 
+let concat= (a,b) =>{
+    let result=a+b;
+
+    return result;
+}
+
+let a;
+a=9;
+let b;
+b=8;
+console.log(concat(a,b));
+
+
+
+
+
 for(let i=1;i<=19;i++)
 {
     console.log("I love Uganda");
